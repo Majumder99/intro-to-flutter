450bdad89dd1d0c617d80e3cb8543ff524f7c1e4
Sourav Majumder <souravmajumderk99@gmail.com>
2023-02-22 11:33:52 +0600

    new things

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ void main() {
   runApp(MaterialApp(
       title: "My flutter app",
       home: Scaffold(
+          appBar: AppBar(title: Text("Dashboard".toUpperCase())),
           body: Center(
-        child: Text(
-          "Hello sourav",
-          textDirection: TextDirection.ltr,
-        ),
-      ))));
+            child: Text(
+              "Hello sourav",
+              textDirection: TextDirection.ltr,
+            ),
+          ))));
 }
