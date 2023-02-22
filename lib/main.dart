45acaef6bb67d93d3b1e518c5c833775b0034d73
Sourav Majumder <souravmajumderk99@gmail.com>
2023-02-22 11:48:15 +0600

    done for now

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,31 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       title: "My flutter app",
       home: Scaffold(
-          appBar: AppBar(title: Text("Dashboard".toUpperCase())),
-          body: Center(
-            child: Text(
-              "Hello sourav",
-              textDirection: TextDirection.ltr,
+        appBar: AppBar(
+          title: Text("Dashboard".toUpperCase()),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Text.rich(
+            TextSpan(
+              text: "Sourav",
+              children: [
+                TextSpan(
+                    text: "Majumder",
+                    style: TextStyle(fontSize: 30.0, color: Colors.red)),
+                TextSpan(
+                    text: "Good",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.black))
+              ],
             ),
-          ))));
+          ),
+        ),
+      ),
+    ),
+  );
 }
